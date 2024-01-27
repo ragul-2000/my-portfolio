8c43990eb2f62caf551f6130a853e08c4c83d72d
ragul-2000 <ragulmyd2000@gmail.com>
2024-01-27 13:58:20 +0530

    update build for gh-pages

--- src/data.jsx
+++ src/data.jsx
@@ -3,11 +3,12 @@ import Social from "../src/img/social.png"
 import Blogs from "../src/img/blogss.png"
 import Booking from "../src/img/booking.png"
 import Agency from "../src/img/dhanam.png"
+import Ecommerce from "../src/img/ecommerce.png"
 export const products = [
     {
       id: 1,
-      img: Booking,
-      link: "https://65a53343fbeb880bba849ac5--remarkable-faloodeh-336040.netlify.app/",
+      img: Ecommerce,
+      link: "https://aquamarine-hamster-e08c5e.netlify.app/",
     },
     {
       id: 2,
@@ -15,24 +16,24 @@ export const products = [
       link: "https://ragul-2000.github.io/Social_App/",
     },
     {
-      id: 3,
+    id: 3,
+    img: Booking,
+    link: "https://65a53343fbeb880bba849ac5--remarkable-faloodeh-336040.netlify.app/",
+    },
+    {
+      id: 4,
       img: Agency,
       link: "https://ragul-2000.github.io/DhanamAgencies/",
     },
     {
-      id: 4,
+      id: 5,
       img: Blogs,
       link: "https://github.com/ragul-2000/blog",
     },
     {
-      id: 5,
+      id: 6,
       img: Fashion,
       link: "https://github.com/ragul-2000",
-    },
-    {
-      id: 6,
-      img: "https://i.pinimg.com/564x/78/08/4c/78084cbc246fba79b7baa7d0b2f1c42c.jpg",
-      link: "https://github.com/ragul-2000/Fashionstore",
     },
   ];
   
